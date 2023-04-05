12e797a2d1c6683737f5316c8ab348a50a16855b
stephen-cusi <magiskcurry@qq.com>
2023-04-05 10:31:19 +0800

    fix3

--- quick-build.sh
+++ quick-build.sh
@@ -5,6 +5,6 @@ git clone https://gitlab.com/LostGamer/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
 git pull
 # If you can't configure the build, try replacing ./ with python3?
-./waf configure -T release --key=mykey.keystore,magisk6666,114514,114514
+./waf configure -T release --key=mykey.keystore,magisk6666,114514,114514 --prefix=./build
 # Pass: Another homo moment
 ./waf build
