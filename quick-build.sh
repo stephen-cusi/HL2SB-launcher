7782771f4d43860683917b58172412ca93a1f553
stephen-cusi <magiskcurry@qq.com>
2023-04-05 10:09:11 +0800

    Update Build Script

--- quick-build.sh
+++ quick-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Purpose: Quickly build a launcher Apk and use my private signature.
+cd srceng-androidwaf
+git clone https://gitlab.com/LostGamer/android-sdk
+export ANDROID_SDK_HOME=$PWD/android-sdk
+git pull
 # If you can't configure the build, try replacing ./ with python3?
 ./waf configure -T release --key=mykey.keystore,magisk6666,114514,114514
 # Pass: Another homo moment
